911e4cccc3b69e0106f566c4f1cacf3629245ba6
Alae Chouiyakh <alaechouiye@gmail.com>
2023-05-23 17:07:05 -0700

    apa

--- shell.c
+++ shell.c
@@ -1,55 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
 
 int main(void) {
-    char *buffer;
-    size_t bufsize = BUFFER_SIZE;
-
-    buffer = (char *)malloc(bufsize * sizeof(char));
-    if (buffer == NULL) {
-        perror("malloc error");
-        exit(EXIT_FAILURE);
-    }
+    char input[BUFFER_SIZE];
 
     while (1) {
-        printf("$ ");
-        if (getline(&buffer, &bufsize, stdin) == -1) {
-            if (feof(stdin)) {
-                printf("\n");
-                break;
-            } else {
-                perror("getline error");
-                exit(EXIT_FAILURE);
-            }
-        }
+        printf("$ ");  // Display the prompt
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL)
+            break;  // Handle end of file (Ctrl+D)
 
-        // Remove newline character from the end
-        buffer[strcspn(buffer, "\n")] = '\0';
+        input[strcspn(input, "\n")] = '\0';  // Remove the trailing newline character
 
-        // Fork a child process
-        pid_t pid = fork();
-
-        if (pid == -1) {
-            perror("fork error");
+        pid_t pid = fork();  // Fork a child process
+        if (pid < 0) {
+            perror("fork");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
             // Child process
-            if (execve(buffer, (char *[]) {buffer, NULL}, NULL) == -1) {
-                perror("execve error");
-                exit(EXIT_FAILURE);
-            }
+            execlp(input, input, NULL);  // Execute the command
+            perror("execlp");  // If execlp returns, an error occurred
+            exit(EXIT_FAILURE);
         } else {
             // Parent process
-            wait(NULL);
+            waitpid(pid, NULL, 0);  // Wait for the child process to complete
         }
     }
-
-    free(buffer);
 
     return 0;
 }
