3d7a36e5ccaa11a016a3732711cd3e66068048e3
Alae Chouiyakh <alaechouiye@gmail.com>
2023-05-24 14:14:43 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+void shell_loop()
+{
+char *input;
+char **args;
+int status;
+
+        do {
+        printf("$ ");
+        input = read_input();
+        args = split_input(input);
+        status = execute(args);
+
+        free(input);
+        free(args);
+}
+        while (status);
+}
+
+int execute(char **args)
+{
+        pid_t pid;
+int status;
+
+        pid = fork();
+        if (pid < 0)
+        {
+        perror("fork");
+        exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+
+        if (execvp(args[0], args) == -1)
+        {
+        perror("execvp");
+        exit(EXIT_FAILURE);
+        }
+        }
+        else
+        {
+        do
+{
+        waitpid(pid, &status, WUNTRACED);
+        }
+        while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+
+        return (1);
+}
