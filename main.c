0db3862475daeb3e575bb20dcc6abd4c769d9330
Alae Chouiyakh <alaechouiye@gmail.com>
2023-05-24 14:36:35 -0700

    ll

--- main.c
+++ main.c
@@ -1,51 +1,53 @@
 #include "shell.h"
 
-void shell_loop()
+void shell_loop(void)
 {
 char *input;
 char **args;
 int status;
 
-        do {
-        printf("$ ");
-        input = read_input();
-        args = split_input(input);
-        status = execute(args);
+	do
 
-        free(input);
-        free(args);
+	{
+	printf("$ ");
+	input = read_input();
+	args = split_input(input);
+	status = execute(args);
+
+	free(input);
+	free(args);
 }
-        while (status);
+	while (status);
 }
 
 int execute(char **args)
 {
-        pid_t pid;
+	pid_t pid;
 int status;
 
-        pid = fork();
-        if (pid < 0)
-        {
-        perror("fork");
-        exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
+	pid = fork();
+	if (pid < 0)
+	{
+	perror("fork");
+	exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
 
-        if (execvp(args[0], args) == -1)
-        {
-        perror("execvp");
-        exit(EXIT_FAILURE);
-        }
-        }
-        else
-        {
-        do
-{
-        waitpid(pid, &status, WUNTRACED);
-        }
-        while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
+	if (execvp(args[0], args) == -1)
+	{
+	perror("execvp");
+	exit(EXIT_FAILURE);
+	}
+	}
+	else
+	{
+	do
 
-        return (1);
+	{
+	waitpid(pid, &status, WUNTRACED);
+	} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return (1);
 }
