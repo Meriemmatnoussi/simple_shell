f5acb9d1993dbd82726f5a1d2f5966780e29201c
Alae Chouiyakh <alaechouiye@gmail.com>
2023-05-24 09:21:41 -0700

    pfffffff

--- shell.c
+++ shell.c
@@ -1,53 +1,7 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 
-void shell_loop()
+int main(void)
 {
-    char *input;
-    char **args;
-    int status;
-
-    do {
-        printf("$ ");
-        input = read_input();
-        args = split_input(input);
-        status = execute(args);
-
-        free(input);
-        free(args);
-    } while (status);
-}
-
-int execute(char **args)
-{
-    pid_t pid;
-    int status;
-
-    pid = fork();
-    if (pid < 0)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        do {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-
-    return 1;
+    shell_loop();
+    return 0;
 }
--- shell.h
+++ shell.h
@@ -1,6 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define TOKEN_BUFFER_SIZE 64
 #define TOKEN_DELIMITERS " \t\r\n\a"
 
