9660cd0a86788e4247bb1aecd155a7f8b4db7096
Alae Chouiyakh <alaechouiye@gmail.com>
2023-05-24 08:35:00 -0700

    this-must-to-be-a-joke

--- shell.c
+++ shell.c
@@ -1,7 +1,45 @@
 #include "shell.h"
 
-int main(void)
+int execute(char **args)
 {
-    shell_loop();
-    return 0;
+    pid_t pid;
+    int status;
+
+    if (strcmp(args[0], "exit") == 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+void shell_loop()
+{
+    char *input;
+    char **args;
+    int status;
+
+    do {
+        printf("$ ");
+        input = read_input();
+        args = split_input(input);
+        status = execute(args);
+
+        free(input);
+        free(args);
+    } while (status);
 }
