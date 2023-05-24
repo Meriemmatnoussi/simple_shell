609ad60b8ce5e521a1a6bd9396804519d335f04b
Alae Chouiyakh <alaechouiye@gmail.com>
2023-05-23 17:29:21 -0700

    third-commid

--- shell.c
+++ shell.c
@@ -6,31 +6,42 @@
 #include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
+/**
+ * main - Simple UNIX command line interpreter.
+ *
+ * Return: Always 0.
+ */
 
-int main(void) {
-    char input[BUFFER_SIZE];
+int main(void)
+{
+char input[BUFFER_SIZE];
 
-    while (1) {
-        printf("$ ");  // Display the prompt
-        if (fgets(input, BUFFER_SIZE, stdin) == NULL)
-            break;  // Handle end of file (Ctrl+D)
+while (1)
+{
+	printf("$ ");
+if (fgets(input, BUFFER_SIZE, stdin) == NULL)
+	break;
 
-        input[strcspn(input, "\n")] = '\0';  // Remove the trailing newline character
+	input[strcspn(input, "\n")] = '\0';
 
-        pid_t pid = fork();  // Fork a child process
-        if (pid < 0) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            execlp(input, input, NULL);  // Execute the command
-            perror("execlp");  // If execlp returns, an error occurred
-            exit(EXIT_FAILURE);
-        } else {
-            // Parent process
-            waitpid(pid, NULL, 0);  // Wait for the child process to complete
-        }
-    }
+	pid_t pid = fork();
 
-    return 0;
+	if (pid < 0)
+	{
+	perror("fork");
+	exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+	execlp(input, input, NULL);
+	perror("execlp");
+	exit(EXIT_FAILURE);
+	}
+	else
+	{
+	waitpid(pid, NULL, 0);
+	}
+	}
+
+return (0);
 }
