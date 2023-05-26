450b514707022229a05aa2ffa728d0e4259e6f18
Alae Chouiyakh <alaechouiye@gmail.com>
2023-05-25 20:52:52 -0700

    im_scrood

--- helpers.c
+++ helpers.c
@@ -1,48 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include "helpers.h"
+#include "shell.h"
 
-void print_prompt(void) {
-    printf("> ");
-    fflush(stdout);
+/**
+ * read_input - Reads a line of input from the user.
+ *
+ * Return: The input string read from the user.
+ */
+char *read_input()
+{
+char *user_input = NULL;
+size_t bufsize = 0;
+
+getline(&user_input, &bufsize, stdin);
+	return (user_input);
 }
 
-char *read_line(void) {
-    char *line = NULL;
-    size_t bufsize = 0;
-    getline(&line, &bufsize, stdin);
-    return line;
+/**
+ * split_input - Splits the input string into an array of tokens.
+ * @user_input: The input string to be split.
+ *
+ * Return: An array of tokens.
+ */
+char **split_input(char *user_input)
+{
+int token_bufsize = TOKEN_BUFFER_SIZE;
+int position = 0;
+char **tokens = malloc(token_bufsize * sizeof(char *));
+char *token;
+
+	if (!tokens)
+	{
+	fprintf(stderr, "Allocation error\n");
+	exit(EXIT_FAILURE);
+	}
+
+	token = strtok(user_input, TOKEN_DELIMITERS);
+	while (token != NULL)
+	{
+	tokens[position] = token;
+	position++;
+
+	if (position >= token_bufsize)
+	{
+	token_bufsize += TOKEN_BUFFER_SIZE;
+	tokens = realloc(tokens, token_bufsize * sizeof(char *));
+	if (!tokens)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	}
+
+	token = strtok(NULL, TOKEN_DELIMITERS);
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
 
-char **split_line(char *line) {
-    int bufsize = TOKEN_BUFSIZE;
-    int position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
-
-    if (!tokens) {
-        fprintf(stderr, "Allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, TOKEN_DELIMITERS);
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize) {
-            bufsize += TOKEN_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "Allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, TOKEN_DELIMITERS);
-    }
-    tokens[position] = NULL;
-    return tokens;
-}
--- shell.c
+++ shell.c
@@ -1,91 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <limits.h>
 #include "shell.h"
-#include "helpers.h"
 
-void shell_loop(void) {
-    char *line;
-    char **args;
-    int status;
-
-    do {
-        print_prompt();
-        line = read_line();
-        args = split_line(line);
-        status = execute(args);
-
-        free(line);
-        free(args);
-    } while (status);
-}
-
-int execute(char **args) {
-    if (args[0] == NULL) {
-        return 1;
-    }
-
-    if (strcmp(args[0], "exit") == 0) {
-        return 0;
-    }
-
-    if (strcmp(args[0], "cd") == 0) {
-        if (args[1] == NULL) {
-            // Change directory to the value of HOME environment variable
-            const char *home_dir = getenv("HOME");
-            if (home_dir == NULL) {
-                fprintf(stderr, "shell: cd: HOME not set\n");
-            } else {
-                if (chdir(home_dir) != 0) {
-                    perror("shell");
-                }
-            }
-        } else if (strcmp(args[1], "-") == 0) {
-            // Change directory to the value of OLDPWD environment variable
-            const char *old_pwd = getenv("OLDPWD");
-            if (old_pwd == NULL) {
-                fprintf(stderr, "shell: cd: OLDPWD not set\n");
-            } else {
-                if (chdir(old_pwd) != 0) {
-                    perror("shell");
-                }
-            }
-        } else {
-            // Change directory to the specified directory
-            if (chdir(args[1]) != 0) {
-                perror("shell");
-            }
-        }
-        // Update the value of PWD environment variable
-        char cwd[PATH_MAX];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            setenv("PWD", cwd, 1);
-        } else {
-            perror("shell");
-        }
-        return 1;
-    }
-
-    pid_t pid, wpid;
-    int status;
-
-    pid = fork();
-    if (pid == 0) {
-        if (execvp(args[0], args) == -1) {
-            perror("shell");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        perror("shell");
-    } else {
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-
-    return 1;
+/**
+ * main - Entry point of the shell program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	shell_loop();
+	return (0);
 }
